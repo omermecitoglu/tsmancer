e08ce64db5e9cb973e9ccc03432fa53e3b89a87b
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-06-16 20:39:20 +0700

    internal: deepEqual utility function

---
+++ src/utils/deepEqual.ts
@@ -0,0 +1,27 @@
+/**
+ * Compares two values for deep equality.
+ *
+ * @param a - The first value to compare.
+ * @param b - The second value to compare.
+ * @returns True if the values are deeply equal, false otherwise.
+ */
+export default function deepEqual(a: unknown, b: unknown): boolean {
+  if (typeof a !== typeof b) return false;
+  switch (typeof a) {
+    case "object": {
+      if (!a || !b) return a === b;
+      if (Array.isArray(a) && Array.isArray(b)) {
+        return a.every((item, index) => deepEqual(item, b[index]));
+      }
+      if (Object.keys(a).length !== Object.keys(b).length) return false;
+      return Object.entries(a).every(([key, value]) => {
+        return deepEqual(value, (b as Record<string, unknown>)[key]);
+      });
+    }
+    case "function":
+    case "symbol":
+      return false;
+    default:
+      return a === b;
+  }
+}
