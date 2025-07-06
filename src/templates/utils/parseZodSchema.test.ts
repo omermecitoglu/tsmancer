c646062d4132256cb46055f0cf55858098c780e8
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-06 11:41:39 +0700

    Revert "fix: disable pre-validation"
    This reverts commit 56ad9f97d85a4614c19105db94acab94671f5ad2.

--- src/templates/utils/parseZodSchema.test.ts
+++ src/templates/utils/parseZodSchema.test.ts
@@ -6,8 +6,8 @@ describe("generateUtilForZod", () => {
     const result = generateUtilForZod();
     expect(result).toContain("import { type ZodType, type z } from \"zod/v4\";");
     expect(result).toContain("export function parseZodSchema");
-    // expect(result).toContain("schema.safeParse(data)");
-    // expect(result).toContain("if (!result.success) throw result.error;");
-    // expect(result).toContain("return result.data;");
+    expect(result).toContain("schema.safeParse(data)");
+    expect(result).toContain("if (!result.success) throw result.error;");
+    expect(result).toContain("return result.data;");
   });
 });
